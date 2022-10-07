2ebc2b9e0b1e66ebf411137c00fc9ee049ea9191
Pavel <paha_gagarin@mail.ru>
2022-10-07 10:25:51 +0300

    пагинация

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "@babel/polyfill";//импортируем полифилы
 import './index.html';//импорт для слежения за html
 import './index.scss';
+import { main } from "./modules/app";
 
 
 window.addEventListener('DOMContentLoaded',function(){
-//стартовый код
+ main()
 })
